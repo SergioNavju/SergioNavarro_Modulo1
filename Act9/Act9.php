32b01ef193f5bbdae993fe35e008e609f739c706
Sergio <sdamian.navarro@gmail.com>
2021-04-29 22:45:55 -0500

    DOCS: Se agregaron comentarios y se modifico la estetica del codigo

--- Act9/Act9.php
+++ Act9/Act9.php
@@ -11,49 +11,58 @@
         <table border="1" >
             <tbody>
             <?php
-                $tamablero = 8;
+                /*Definimos variables que facilitan el poner las imagenes y 
+                aquella que controla el tamaño del tablero*/
+                $tamtablero = 8;
                 $cNegro = "<img src=\"./negro.jpg\" width=\"50\" height=\"50\">";
                 $cBlanco = "<img src=\"./blanco.jpg\" width=\"50\" height=\"50\">";
-                for ($n=1; $n<=$tamablero; $n++)
+
+                //Se lleva un control de filas totales
+                //Estructura que itera segun el valor de tamtablero
+                for ($n=1; $n<=$tamtablero; $n++)
                 {
                     echo "<tr>";
+                        //Se lleva un control sobre pares y nones para poder alternar el color de las casillas
+                        //Se imprimen las filas que empiezan con negro
                         if ($n%2 != 0)
-                        {
-                            for ($i=1; $i<=$tamablero; $i++)
+                        {   //Se lleva un control de columnas
+                            for ($i=1; $i<=$tamtablero; $i++)
                             {
-                                
+                                //Se buscan los nones para poder alternar el color de las casillas
                                 if($i%2 != 0)
                                 {
-                                    echo "<td width=\"50\" height=\"50\">";
+                                    echo "<td>";
                                     echo $cNegro;
                                     echo "</td>";   
                                 }
                                 else
                                 {
-                                    echo "<td width=\"50\" height=\"50\">";
+                                    echo "<td>";
                                     echo $cBlanco;
                                     echo "</td>";
                                 }
                             }
-                        }  
-                        if ($n%2 == 0)
+                        }
+                        //Se imprimen las filas que empiezan con blanco
+                        else if ($n%2 == 0)
                         {
-                            for ($i=1; $i<=$tamablero; $i++)
+                            //Se lleva un control de columnas
+                            for ($i=1; $i<=$tamtablero; $i++)
                             {
-                            
+                                
                                 if($i % 2 != 0)
                                 {
-                                    echo "<td width=\"50\" height=\"50\">";
+                                    echo "<td>";
                                     echo $cBlanco;
                                     echo "</td >";
                                 }
+                                //Se buscan los pares para poder alternar el color de las casillas
                                 else
                                 {
-                                    echo "<td width=\"50\" height=\"50\">";
+                                    echo "<td>";
                                     echo $cNegro;
                                     echo "</td>";
                                 }
-                            
                             }  
                         }
                     echo "</tr>";

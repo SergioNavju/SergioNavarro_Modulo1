e05c9a9ddf96b5a4c681a54f9dbc520539369478
Sergio <sdamian.navarro@gmail.com>
2021-05-03 01:42:53 -0500

    FEAT

--- Act13/Act13.php
+++ Act13/Act13.php
@@ -47,35 +47,122 @@
                 echo "</tbody>";
             echo "</table>";
                                                                                     //Barcos
-                    //Mientras sea mayor que 8
-                    $EJEX = ["@","A","B","C","D","E","F","G","H","I","J"];
+
+                    //TENEMOS QUE HACER ARREGLOS PARA PODER CONCOER LA POSICION DE LOS BARCOS
+                    $letras = ["@","A","B","C","D","E","F","G","H","I","J"];
+
                     $EJEY = [0,1,2,3,4,5,6,7,8,9,10];
+                    $EJEX = [
+                        "@" => "0",
+                        "A" => "1",
+                        "B" => "2",
+                        "C" => "3",
+                        "D" => "4",
+                        "E" => "5",
+                        "F" => "6",
+                        "G" => "7",
+                        "H" => "8",
+                        "I" => "9",
+                        "J" => "10",
+                                    ];
+                                    //Crear variable X de los que ingrese el usuario
+                    /*foreach ($EJEX as $key => $value) 
+                    {
+                        $x[$value];
+                    }*/
+                        //barco de 4
 
-                    do{
-                        //Para las filas
-                        $i=rand(1,10);
-                        //Para las columnas
-                        $j=rand(1,10);
-                        if ($i<8)
-                            $barco4h =[ $i,$i+1,$i+2,$i+3];
+                        do {
+                                $i=rand(1,10);
+                                $j=rand(1,10);
+                                if ($i<8) 
+                                {
+                                    //El barco se hace vertical
+                                    $barco4h =[$i,$i+1,$i+2,$i+3];
+                                    $barco4v = [$j];
+                                    $barco4 =[];
+                                    for ($u=0; $u<count($barco4h); $u++)
+                                    {
+                                        array_push($barco4,strval($j) .strval($barco4h[$u]));
+                                    }
+                                    echo "BARCO 4 VERTICAL ";
+                                    print_r($barco4);
+                                    //var_dump ($barco4);
+                                    /*echo "Antes de la asignación de letra";
+                                    var_dump($barco4v);
+                                        $barco4v = $letras[$j];
+                                    echo "Después de la asignación de letra";
+                                    var_dump($barco4v);*/
+                                    
+                                } 
+                                //El barco se hace horizontal
+                                elseif ($j<8)
+                                {
+                                    $barco4v =[$j,$j+1,$j+2,$j+3];
+                                    $barco4h = [$i];
+                                    $barco4 =[];
+                                    for ($u=0; $u<count($barco4v); $u++)
+                                    {
+                                        array_push($barco4, strval($barco4v[$u]). strval($i) );
+                                    }
+                                    /*echo count($barco4h);
+                                    echo "<br>";*/
+                                    echo "BARCO 4 HORIZONTAL ";
+                                    //var_dump ($barco4);
+                                    print_r($barco4);
+                                    //$barco4 = array_merge($barco4h, $barco4v);
+                                } 
+                            } while ($i>=8 && $j>=8);
+                            //barco de 3
+                        do {
+                                $i=rand(1,10);
+                                $j=rand(1,10);
+                                //El barco se hace vertical
+                                if ($i<9) 
+                                {
+                                    $barco3h =[ $i,$i+1,$i+2];
+                                    $barco3v = [$j];
+                                    $barco3 =[];
+                                    for ($u=0; $u<count($barco3h); $u++)
+                                    {
+                                        array_push($barco3,strval($j) .strval($barco3h[$u]));
+                                    }
+                                    echo "BARCO 3 Vertical";
+                                    //var_dump ($barco3);
+                                    print_r($barco3);
+                                    //$barco3 = array_merge($barco3h, $barco3v);
+                                }
+                                //El barco se hace horizontal
+                                elseif ($j<9) 
+                                {
+                                    $barco3v =[ $j,$j+1,$j+2];
+                                    $barco3h =[$i];
+                                    $barco3 =[];
+                                    for ($u=0; $u<count($barco3v); $u++)
+                                    {
+                                        array_push($barco3,strval($barco3v[$u]). strval($i));
 
-                        elseif ($j<8)
-                            $barco4v =[ $j,$j+1,$j+2,$j+3];
-                        
-                    } while ($i>8 && $j>8);
+                                    }
+                                    echo "BARCO 3 Horizontal";
+                                    //var_dump ($barco3);
+                                    print_r($barco3);
 
-                    //barco de 3
-                    do{
-                        $i=rand(1,10);
-                        $j=rand(1,10);
-                        if ($i<9)
-                            $barco3h =[ $i,$i+1,$i+2];
-
-                        elseif ($j<9)
-                            $barco3v =[ $j,$j+1,$j+2];
-
-                        } while ($i>9 && $j>9);
-                                                                    //Verificar Coliciones
+                                    //$barco3 = array_merge($barco3h, $barco3v);
+                                }
+                                else
+                                    $barco3=$barco4;
+                                //for($k=0; $k <count($barco4); )
+                                
+                                $subchoque = array_intersect ($barco4, $barco3);
+                                var_dump($subchoque);
+                                
+                            } while ($i>=9 && $j>=9 && empty ($subchoque) == false);
+                            
+                            
+                            //print_r($barco3); 
+                            echo "<br>";
+                            //print_r($barco4);
+                                //Verificar Coliciones
                                 //Usar array_intersect_assoc
             ?> 
             <br>
@@ -83,6 +170,11 @@
                 Posición Y(Número): <input type="text" name="Y" required>
                 
                 <input type ="submit" name ="Traducit" value="Disparar!!">
+                <?php
+
+                    $x= $_POST["X"];
+                    $y= $_POST["Y"];
+                ?>
     </body>
 </html>
 
